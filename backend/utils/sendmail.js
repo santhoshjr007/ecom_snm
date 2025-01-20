bbb4720ee97c551d725a89c2531cc869875042c9
santhoshjr007 <santhosh.r@kalvium.community>
2025-01-20 16:22:00 +0530

    sendmailer

--- backend/utils/sendmail.js
+++ backend/utils/sendmail.js
@@ -1,1 +1,23 @@
 const nodemailer = require('nodemailer')
+const transporter = nodemailer.createTransport({
+    secure : true,
+    host: 'smtp.gmail.com',
+    port:465,
+    auth:{
+        user:'santhosh.r@kalvium.community',
+        pass:'fxpd kbsg nvnl fzdz',
+    }
+})
+
+function sendMail(to,sub,msg){
+    transporter.sendMail({
+        to:to,
+        subject:sub,
+        html:msg
+    })
+    console.log("mail sent");
+
+}
+
+sendMail("santhosh.r@kalvium.community","This is my subject","This is my mail working");
+module.exports = transporter;
